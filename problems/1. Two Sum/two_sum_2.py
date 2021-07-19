3f27bf6253434857c57c545efe88d524ed91b3b1
ganiirsyadi <ganiilhamirsyadi@gmail.com>
2021-07-19 12:03:34 +0700

    Add commenting

--- problems/1. Two Sum/two_sum_2.py
+++ problems/1. Two Sum/two_sum_2.py
@@ -7,9 +7,18 @@ class Solution:
 
 		# enumerates return index, value pair of a list
 		for index, element in enumerate(nums):
+
+			# find other value so that the sum match to target
 			diff = target - element
+			
+			# pada posisi ini diff pasti lebih kecil, 
+			# misal target = 5 untuk list [1,4]
+			# ketika menemukan 1, pasti 4 belum ada di dict, tetapi ketika menemukan 4, 1 (diff) sudah ada di dict
+			# makanya untuk return dibalik, index diff terlebih dahulu baru yang current index
 			if diff in value_index_dict:
 				return [value_index_dict[diff], index]
+
+			# jika belum ada pasangannya maka tambahkan ke dict (value: index)
 			value_index_dict[element] = index
 	
 	def main(self):
